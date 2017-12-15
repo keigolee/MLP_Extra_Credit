8386eb873b71ff4f4a8bd630501156348ef7535f
keigo.mingli <33563034+keigolee@users.noreply.github.com>
2017-12-15 10:19:56 -0500

    change port number

--- hello_user.py
+++ hello_user.py
@@ -3,13 +3,12 @@ import tornado.web
 
 class HelloUserHandler(tornado.web.RequestHandler):
     def get(self):
-	user = self.get_argument('username')
-	write_str = "Hello, " + user
-	self.write(write_str)
+        user = self.get_argument('username')
+        write_str = "Hello, " + user
+        self.write(write_str)
 
 if __name__ == "__main__":
     handler_mapping = [(r'/hello', HelloUserHandler),]
     app = tornado.web.Application(handler_mapping)
-    app.listen(8006)
+    app.listen(7777)
     tornado.ioloop.IOLoop.current().start()
-
